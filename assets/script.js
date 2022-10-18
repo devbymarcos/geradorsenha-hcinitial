ae267d38ccc062c113ec83b5407281263fe985ee
Marcos <marcoslopes.dev@gmail.com>
2022-10-17 22:16:16 -0300

    add texto do centro

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
+const inputPassword = document.getElementById("inputPassword");
+const inputNumber = document.getElementById("inputNumber");
+const btnUp = document.getElementById("btnUp");
+const btnDown = document.getElementById("btnDown");
 
+const btnCreatePasswd = document.getElementById("gerarSenha");
+const btnCopy = document.getElementById("copy");
