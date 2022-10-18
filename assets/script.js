c2b6bdd8ee9ae845c160f99918fdaaaf51725afb
Marcos <marcoslopes.dev@gmail.com>
2022-10-18 16:54:34 -0300

    criado funcoes para aumentar e diminuir o campo inputNumber

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,24 @@ const btnDown = document.getElementById("btnDown");
 
 const btnCreatePasswd = document.getElementById("gerarSenha");
 const btnCopy = document.getElementById("copy");
+
+const defaultNumber = 8;
+const minChar = 4;
+const char = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJLMNOPQRSTUVWXYZ!@#$%^&*()+?><:{}[]"
+
+inputNumber.value = defaultNumber;
+
+function incrementValue() {
+  let inputValue = inputNumber.value;
+  inputValue++;
+  inputNumber.value = inputValue;
+}
+
+function decrementValue() {
+  let inputValue = inputNumber.value;
+  inputValue--;
+  inputNumber.value = inputValue;
+}
+
+btnUp.addEventListener("click", incrementValue);
+btnDown.addEventListener("click", decrementValue);
